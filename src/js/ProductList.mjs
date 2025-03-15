28d352013f2eddfc806ff4db2037520c45c11de5
Stacy Yarrington <syarrington@enuit.com>
2025-03-15 13:39:41 -0600

    Week 2 Individual

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,33 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product)
+{
+    return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+    <img src="${product.Image}"
+    alt="${product.Name}"/>
+    <h3 class="card__brand">${product.Brand.Name}</h3>    
+    <h2 class="card__name">${product.Name}</h2>
+    <p class="product-card__price">$${product.ListPrice}</p></a>
+    </li>
+    `
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+      this.category = category;
+      this.dataSource = dataSource;
+      this.listElement = listElement;
+    }
+    async init() {
+        // the dataSource will return a Promise...so you can use await to resolve it.
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+        // next, render the list – ** future **
+      }
+    renderList(list)
+    {
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+    
+}
