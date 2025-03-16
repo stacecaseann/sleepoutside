be97461834e800c40a56365512983854d97fa3ba
Stacy Yarrington <syarrington@enuit.com>
2025-03-15 21:57:01 -0600

    week 2 run lint and format and make single quotes be the default

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,13 @@ import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
 import CartTotal from './CartTotal.mjs';
 import { getLocalStorage } from './utils.mjs';
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const listing = new ProductList("Tents", dataSource, element);
+const dataSource = new ProductData('tents');
+const element = document.querySelector('.product-list');
+const listing = new ProductList('Tents', dataSource, element);
 
 listing.init();
 
 //Set cart total from storage
-const cartTotal =  CartTotal.getInstance();
+const cartTotal = CartTotal.getInstance();
 const cart = getLocalStorage('so-cart') || [];
 cartTotal.updateTotal(cart.length);
