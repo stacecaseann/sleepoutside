1ccef31eaab71f758337a875a170db9c84443b3a
Stacy Yarrington <syarrington@enuit.com>
2025-03-29 19:06:44 -0600

    week 4

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -1,9 +1,37 @@
-import { getCartTotal } from "./utils.mjs";
+import { getCartTotal, getLocalStorage } from "./utils.mjs";
+import ExternalServices from "./ExternalServices.mjs";
+
+const services = new ExternalServices();
+
+function formDataToJSON(formElement) {
+  // convert the form data to a JSON object
+  const formData = new FormData(formElement);
+  const convertedJSON = {};
+  formData.forEach((value, key) => {
+    convertedJSON[key] = value;
+  });
+  return convertedJSON;
+}
+
+function packageItems(items) {
+  const simplifiedItems = Object.values(items).map((item) => {
+    console.log(item);
+    return {
+      id: item.Id,
+      price: item.FinalPrice,
+      name: item.Name,
+      quantity: 1,
+    };
+  });
+  return simplifiedItems;
+}
+
+
 
 export default class CheckoutProcess {
-    constructor(key, outputSelector) {
-      this.key = key;
-      this.outputSelector = outputSelector;
+    constructor(/*key, outputSelector*/) {
+      // this.key = key;
+      // this.outputSelector = outputSelector;
       this.list = [];
       this.itemTotal = 0;
       this.shipping = 0;
@@ -12,7 +40,7 @@ export default class CheckoutProcess {
     }
   
     init() {
-      this.list = getLocalStorage(this.key);
+      this.list = getLocalStorage('so-cart');
       this.calculateItemSummary();
     }
     calculateItemSummary()
@@ -24,6 +52,7 @@ export default class CheckoutProcess {
     calculateItemSubTotal() {
       // calculate and display the total dollar amount of the items in the cart, and the number of items.
       let ttl = 0;
+      const cart = this.list;
       for (let key in cart)
       {
         const qty = cart[key].qty;
@@ -35,7 +64,7 @@ export default class CheckoutProcess {
     calculateOrderTotal() {
       // calculate the tax and shipping amounts. Add those to the cart total to figure out the order total
       this.tax = (this.itemTotal *.06);
-      const ttlItems = getCartTotal();
+      const ttlItems = getCartTotal(this.list);
 
       this.shipping = 10 + (ttlItems-1)*2;
       this.orderTotal = this.itemTotal + this.tax + this.shipping;
@@ -50,11 +79,32 @@ export default class CheckoutProcess {
       //tax.innerText = `$${this.tax.toFixed(2)}`;
       //const tax 
       const totalElement = document.querySelector('.checkout');
-      const ttlTemplate = `<div>Subtotal</div><div>${this.itemTotal}
-      <div>Tax</div><div>${this.tax}</div>
-      <div>Shipping Estimate</div><div>${this.shipping}</div>
-      <div>Order Total</div><div>${this.orderTotal}</div>
+      const ttlTemplate = `<div>Subtotal</div><div>$${this.itemTotal}</div>
+      <div>Tax</div><div>$${this.tax}</div>
+      <div>Shipping Estimate</div><div>$${this.shipping}</div>
+      <hr class="line"><hr class="line">
+      <div>Order Total</div><div>$${this.orderTotal}</div>
       `;
       totalElement.innerHTML = ttlTemplate;
     }
+
+    
+  async checkout() {
+    const formElement = document.forms["checkout"];
+    const order = formDataToJSON(formElement);
+
+    order.orderDate = new Date().toISOString();
+    order.orderTotal = this.orderTotal;
+    order.tax = this.tax;
+    order.shipping = this.shipping;
+    order.items = packageItems(this.list);
+    //console.log(order);
+
+    try {
+      const response = await services.checkout(order);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
   }
+}
